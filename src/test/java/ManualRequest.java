05e98e36d3316ff419d65a731144bccf57ae81e9
vivkekanandd1 <vivekanandd1@live.com>
2025-01-07 20:44:13 +0530

    Commit 4:Market and Certificate type selection made

--- src/test/java/ManualRequest.java
+++ src/test/java/ManualRequest.java
@@ -2,7 +2,9 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class ManualRequest {
 	
@@ -15,6 +17,14 @@ public class ManualRequest {
 		driver.findElement(By.id("kreditz_current_password")).sendKeys("Password123");
 		driver.findElement(By.cssSelector("button[type='submit']")).click();
 		driver.findElement(By.cssSelector("span.menu-icon-new-request")).click();
+		driver.findElement(By.id("recipient_name")).sendKeys("Bruce Wayne");
+		driver.findElement(By.id("e-post")).sendKeys("snaps.deshmukh@gmail.com");
+		WebElement types = driver.findElement(By.cssSelector("select[name='type']"));
+		Select slct = new Select(types);
+		slct.selectByValue("customer");
+		WebElement Markets=driver.findElement(By.id("request-country-select"));
+		Select slct1 = new Select(Markets);
+		slct1.selectByValue("1");
 		
 	}
 
