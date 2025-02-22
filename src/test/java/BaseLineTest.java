ddb314dc3aae70eeebeaae4eee0995a603d8a089
vivkekanandd1 <vivekanandd1@live.com>
2025-02-22 22:30:54 +0530

    Windows handle

--- src/test/java/BaseLineTest.java
+++ src/test/java/BaseLineTest.java
@@ -1,47 +1,58 @@
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-
 
 public class BaseLineTest {
-	
-	String URL = "https://pre-vista.kreditz.com/login";
-	String Email = "snaps.deshmukh@gmail.com";
-	String Password = "Password123";
-	
+
 	WebDriver driver;
 	WebDriverWait wait;
 	public Datas data;
-	
-	
+
 	public WebDriver start() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		wait= new WebDriverWait(driver, Duration.ofMinutes(15));
+		wait = new WebDriverWait(driver, Duration.ofMinutes(15));
 		return driver;
-		
 	}
-	
-	
+
 	@BeforeMethod(alwaysRun = true)
 	public Datas launchApp() {
 		driver = start();
 		data = new Datas(driver, wait);
-		data.Login();
 		return data;
 	}
-	
-	
+
 	@AfterMethod
 	public void teardown() {
-		driver.quit();  
+		driver.quit();
+	}
+	
+	public void WindowShuffleChild() {
+		  Set<String> Wind = driver.getWindowHandles();
+	        Iterator<String> Winds = Wind.iterator();
+	        String Parent = Winds.next();
+	        String Child = Winds.next();
+	        driver.switchTo().window(Child);
+	}
+	
+	public void WindowShuffleParent() {
+		  Set<String> Wind = driver.getWindowHandles();
+	        Iterator<String> Winds = Wind.iterator();
+	        String Parent = Winds.next();
+	        String Child = Winds.next();
+	        driver.switchTo().window(Parent);
+	}
+	
+	public void Pageload() throws InterruptedException {
+        Thread.sleep(2000);
+
 	}
 
 }
