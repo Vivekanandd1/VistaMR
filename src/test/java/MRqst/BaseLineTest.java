56875750dd31c864671f35a1453d65138c51e4b9
vivkekanandd1 <vivekanandd1@live.com>
2025-06-06 16:16:25 +0530

    Added the different browser drivers

--- src/test/java/MRqst/BaseLineTest.java
+++ src/test/java/MRqst/BaseLineTest.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,12 +17,24 @@ public class BaseLineTest {
 	WebDriver driver;
 	WebDriverWait wait;
 	public Datas data;
+	
 
 	public WebDriver start() {
-		ChromeOptions opt = new ChromeOptions();
-		opt.addArguments("guest");
-		opt.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-		driver = new ChromeDriver(opt);
+    /*Chrome setup*/
+//		ChromeOptions opt = new ChromeOptions();
+//		opt.addArguments("guest");
+//		opt.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+//		driver = new ChromeDriver(opt);
+		
+//		EdgeOptions opt = new EdgeOptions();
+//		opt.addArguments("guest");
+//		opt.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+//		driver = new EdgeDriver(opt);
+		
+		/*This is something we have to make more dynamic*/
+		
+		driver = new FirefoxDriver();
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		wait = new WebDriverWait(driver, Duration.ofMinutes(15));
