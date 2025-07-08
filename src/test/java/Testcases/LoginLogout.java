55b376b0d40901846fb8bdd8d764d7455f03bb80
vivkekanandd1 <vivekanandd1@live.com>
2025-07-08 07:18:23 +0530

    Login method rectified

--- src/test/java/Testcases/LoginLogout.java
+++ src/test/java/Testcases/LoginLogout.java
@@ -1,16 +1,24 @@
 package Testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import AbstractComponent.BaseLineTest;
 import DataSources.DataProviders;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
 
 public class LoginLogout extends BaseLineTest{
 	
+	
+	@Description("This test case is for Login/Logout functinality")
+    @Owner("Vivekanand Deshmukh")
 	@Test(dataProvider = "CredsDB", dataProviderClass = DataProviders.class)
-	public void LogInOut(String Email, String Password,String Name) throws InterruptedException {
+	public void LogInOut(String Email, String Password) throws InterruptedException {
 		data.Login(Email, Password);
 		data.Logout();
+		String ActualTitles = driver.getTitle();
+		Assert.assertEquals(ActualTitles, "Kreditz | Vista");
 		
 	}
 
