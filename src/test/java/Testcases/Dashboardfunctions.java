68e9f0ad8f032e961b1bace4b6ebdc0641967850
vivekanandcodenote-pixel <vivekanand.codenote@gmail.com>
2026-01-14 20:55:50 +0530

    Dashboard Feature test

---
+++ src/test/java/Testcases/Dashboardfunctions.java
@@ -0,0 +1,23 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+
+import AbstractComponent.BaseLineTest;
+import DataSources.DataProviders;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+
+public class Dashboardfunctions extends BaseLineTest {
+	
+	String Certificatenumber = "51818";
+	
+	@Description("This test case is for Dashboard Search and Filter Functionalities")
+    @Owner("Vivekanand Deshmukh")
+	@Test(priority = 0,dataProvider = "driver", dataProviderClass = DataProviders.class)
+	public void UserCreation(String Email, String Password) throws InterruptedException {
+		data.Login(Email, Password);	
+		CrtfctLgs.CertificateSearch(Certificatenumber);
+		
+	}
+
+}
